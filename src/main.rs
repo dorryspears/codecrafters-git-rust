6f19cd7bd0a12b16d803dab37e1e5dda9bd8f0e4
Ryan <18rspears@gmail.com>
2023-09-20 18:54:32 -0400

    test test

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,6 @@ use sha1::Sha1;
 use sha1::Digest;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     //Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
 
